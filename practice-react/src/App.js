e4f94164619d78b5db47d4f24ad95d6a56b7f292
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2025-01-20 23:43:32 -0500

    Very easy completed

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Info</h1>
+        <p>Name: Anthony Montesdeoca</p>
+        <p>Date of Birth: 08-27-01</p>
+      </div>
+    );
+  }
 }
 
 export default App;
